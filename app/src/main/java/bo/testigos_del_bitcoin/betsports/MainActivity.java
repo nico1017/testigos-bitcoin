d5939245cea95ace67aaf73f0faf8a6b1cb1ff65
Maveto13 <maveto13@gamil.com>
2019-04-18 08:44:22 -0400

    first commit

---
+++ app/src/main/java/bo/testigos_del_bitcoin/betsports/MainActivity.java
@@ -0,0 +1,13 @@
+package bo.testigos_del_bitcoin.betsports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

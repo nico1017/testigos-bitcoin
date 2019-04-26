bbf30024a9d17c36e47681d2c838bc0bc31c03d4
adrianM1498 <adrianmontanho1234@gmail.com>
2019-04-25 22:09:58 -0400

    avance diseño de pantalla para deportes

--- app/src/main/java/bo/testigos_del_bitcoin/betsports/ChooseSportActivity.java
+++ app/src/main/java/bo/testigos_del_bitcoin/betsports/ChooseSportActivity.java
@@ -1,4 +1,18 @@
 package bo.testigos_del_bitcoin.betsports;
 
-public class ChooseSportActivity {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+public class ChooseSportActivity extends AppCompatActivity {
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_sport);
+
+          LinearLayout padre;
+    }
 }

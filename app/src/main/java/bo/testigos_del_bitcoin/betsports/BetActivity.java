be84a9ae7f8cac84908d59b4780173fab76a9ba7
Nico10 <nicobarfer@gmail.com>
2019-04-28 11:21:24 -0400

    segundo commit

---
+++ app/src/main/java/bo/testigos_del_bitcoin/betsports/BetActivity.java
@@ -0,0 +1,20 @@
+package bo.testigos_del_bitcoin.betsports;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class BetActivity {
+    private static final String LOG = BetActivity.class.getName();
+    private Context mContext;
+    private LinearLayout botones;
+    private Button jugar;
+
+    public BetActivity(Context mContext) {
+        this.mContext = mContext;
+    }
+
+
+
+}

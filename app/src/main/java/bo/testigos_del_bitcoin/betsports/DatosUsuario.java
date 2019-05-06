99cdd1b0f3d78bdde30db2e6cf7e29e4321d83a2
adrianM1498 <adrianmontanho1234@gmail.com>
2019-05-06 09:14:14 -0400

    pantalla usuario

---
+++ app/src/main/java/bo/testigos_del_bitcoin/betsports/DatosUsuario.java
@@ -0,0 +1,13 @@
+package bo.testigos_del_bitcoin.betsports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatosUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_usuario);
+    }
+}
